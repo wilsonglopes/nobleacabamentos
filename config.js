646c9895cdfd4ac97f9c17c2888e29d9068c7084
wilsonglopes <wilsonglopes@gmail.com>
2026-03-03 10:15:37 -0300

    refactor: centralize supabase config and cleanup project for github

---
+++ config.js
@@ -0,0 +1,8 @@
+// Noble Acabamentos - Central Client Configuration
+const CONFIG = {
+    SUPABASE_URL: 'https://xpoktmqxmgduginmqojf.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwb2t0bXF4bWdkdWdpbm1xb2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODUyODMsImV4cCI6MjA4NzM2MTI4M30.Zkn5oXvHzqAvOyIrEUQeRLFto_dibEQo9ynBqtidQv8'
+};
+
+// Export for use in HTML files
+window.CONFIG = CONFIG;
